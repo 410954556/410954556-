f9d69d0c9625f0c34eb1814cdcde1fe579f81767
410954556 <s1095455>
2022-06-16 20:00:05 +0800

    2

---
+++ 410954556期末/Controllers/AboutController.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace _410954556期末.Controllers
+{
+    public class AboutController : Controller
+    {
+        // GET: Default
+        public ActionResult Index()
+        {
+            return View();
+        }
+        public ActionResult Test()
+        {
+            return View();
+        }
+        public ActionResult Time()
+        {
+            return View();
+        }
+    }
+}
